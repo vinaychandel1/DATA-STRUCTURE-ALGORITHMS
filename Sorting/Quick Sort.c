d62c1b1bcb45c131e7cadf9ec6dbd1c8860fdc79
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-03-04 00:30:45 +0530

    Update Quick Sort.c

--- Sorting/Quick Sort.c
+++ Sorting/Quick Sort.c
@@ -1,42 +1,42 @@
 #include<stdio.h>
 #include<algorithm>
-
-using namespace std;
 void swap(int* a, int* b) 
 { 
     int t = *a; 
     *a = *b; 
     *b = t; 
 }
-int partition(int arr[],int start,int end){
-    int pivot=arr[end],pIndex=start;
-    int i;
-    for(i=start;i<end;i++){
-        if(arr[i]<pivot){
-            swap(&arr[i], &arr[pIndex]);
+int partition (int arr[], int start, int end) 
+{ 
+    int pivot = arr[end]; 
+    int pIndex = start-1; 
+ 
+    for (int i = start; i <end; i++) 
+    { 
+        if (arr[i] < pivot) 
+        { 
             pIndex++;
-        }
-    }
-    swap(&arr[pivot],&arr[pIndex]);
-    return pIndex;
-}
-void quickSort(int arr[],int start, int end){
-    while(start<end){
-        int pIndex=partition(arr,0,end);
- partition(arr,0,pIndex-1);
- partition(arr,pIndex+1,end);
-    }
-    
-}
-
+            swap(&arr[pIndex], &arr[i]); 
+        } 
+    } 
+    swap(&arr[pIndex+1], &arr[end]); 
+    return pIndex+1; 
+} 
+void quickSort(int arr[], int start, int end) 
+{ 
+    if (start < end) 
+    {
+        int pi = partition(arr, start, end); 
+        quickSort(arr, start, pi - 1); 
+        quickSort(arr, pi + 1, end); 
+    } 
+} 
 int main()
 {
  int arr[]={6,5,7,8,3,9,12,17};
  int size=sizeof(arr)/sizeof(arr[0]);
  quickSort(arr,0,size-1);
- //printArray(arr, size);
  for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) 
-        printf("%d",arr[i]); 
-    printf("Hello");
+        printf("%d ",arr[i]); 
    return 0;
 }
