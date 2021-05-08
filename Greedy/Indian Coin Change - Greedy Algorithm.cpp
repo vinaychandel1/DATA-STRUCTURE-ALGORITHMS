252fcf8c75a1cfdb87fece44fe1dd73aeeada842
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-05-08 23:34:36 +0530

    Update Indian Coin Change - Greedy Algorithm.cpp

--- Greedy/Indian Coin Change - Greedy Algorithm.cpp
+++ Greedy/Indian Coin Change - Greedy Algorithm.cpp
@@ -26,3 +26,37 @@ int main()
 
     return 0;
 }
+/*
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    vector<ll> v;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end(), greater<int>());
+    ll x;
+    cin >> x;
+    ll ans = 0,prevans=0;
+    for (int i = 0; i < n; i++)
+    {
+        ans += x / arr[i];
+        if(ans!=prevans){
+        	cout<<arr[i]<<"\t";
+        	prevans=ans;
+        }
+        
+        x -= x / arr[i] * arr[i];
+    }
+    cout << ans << endl;
+
+    return 0;
+}
+*/
