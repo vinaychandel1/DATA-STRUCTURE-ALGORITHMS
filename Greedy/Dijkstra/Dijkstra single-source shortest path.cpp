f6a40bca5feeaf385043583e2815ca347e51c8a7
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-07-17 00:15:41 +0530

    Create Dijkstra single-source shortest path.cpp

---
+++ Greedy/Dijkstra/Dijkstra single-source shortest path.cpp
@@ -0,0 +1,56 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+const int inf=1e7;
+int main()
+{
+    int n,m;cin>>n>>m;//n=verices,m=edges
+    vector<int> dist(n+1,inf);//assigned all edges infinity
+    vector<vector<pair<int,int>>> graph(n+1);
+    for (int i = 0; i < m; ++i)
+    {
+        int u,v,w;
+        cin>>u>>v>>w;
+        graph[u].push_back({v,w});
+        graph[v].push_back({u,w});
+    }
+    int source;cin>>source;
+    dist[source]=0;
+    set<pair<int,int>>s;
+    //{wt,vertex}
+    s.insert({0,source});
+    while(!s.empty()){
+        auto x=*(s.begin());
+        s.erase(x);
+        for (auto it:graph[x.second])
+        {
+            if (dist[it.first]>dist[x.second]+it.second)
+            {
+                s.erase({dist[it.first],it.first});
+                dist[it.first]=dist[x.second]+it.second;
+                s.insert({dist[it.first],it.first});
+            }
+        }
+    }
+    for (int i = 1; i <=n; ++i)
+    {
+        if(dist[i]<inf){
+            cout<<dist[i]<<" ";
+        }
+        else{
+            cout<<-1<<" ";
+        }
+    }
+    return 0;
+}
+/*
+Input:
+4 4
+1 2 24
+1 4 20
+3 1 3 
+4 3 12
+1
+Output:
+0 24 3 15 
+*/
