639bedfb3684908c349d83b1b52db77f43a2542d
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2022-05-02 22:14:53 +0530

    Create Disjoint Set Union (DSU).cpp

---
+++ Disjoint Set Union (DSU)/Disjoint Set Union (DSU).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N+1e5+10;
+int parent[N];
+
+void make(int v){
+    visited[v]=v;
+}
+int find(int v){
+    if(v==parent[v]) return v;
+    return parent[v]=find(parent[v]); //path compression
+} 
+void Union(int a,int b){
+    a=find(a);
+    b=find(b);
+    if(a!=b){
+        //union by size
+        if(size[a]<size[b])
+            swap(a,b);
+        parent[b]=a;
+        size[a]+=size[b;]
+    }
+}
+
+int main(){
+    int n,k; //n=node.k=edges
+    cin>>n>>k;
+    for (int i = 1; i <=n; ++i)
+    {
+        make(i);
+    }
+    while(k--){
+        int u,v;
+        cin>>u>>v;
+        Union(u,v);
+    }
+    int connected_ct=0;
+    for (int i = 1; i <=n; ++i)
+    {
+       if(find(v)==v) connected_ct++;
+    }
+}
+
+//https://www.youtube.com/watch?v=zEAmQqOpfzM
